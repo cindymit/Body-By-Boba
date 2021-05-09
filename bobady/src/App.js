c51e5220f430f31d26950510e5f65fda963eb9dc
Cindy Mit <cindymit@Cindys-MacBook-Air.local>
2021-05-09 19:51:14 -0400

    Added env, index js within services, useEffect and useState for bobas

--- bobady/src/App.js
+++ bobady/src/App.js
@@ -1,13 +1,28 @@
 import { Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { baseURL, config } from "./services";
 import Nav from "./components/Nav";
 import './App.css';
 
 function App() {
+  const [bobas, setBobas] = useState([]);
+
+  useEffect(() => {
+    const fetchBobas = async () => {
+      const resp = await axios.get(baseURL, config);
+      setBobas(resp.data.records);
+    }
+    fetchBobas();
+  }, []);
+
   return (
     <div className="App">
       <Nav />
       <Route exact path="/">
-        <h3>This is the home page!</h3>
+        <main>
+          <h3>This is the home page!</h3>
+        </main>
       </Route>
       <Route path="/new">
         <h3>Create-A-Drink here!</h3>
