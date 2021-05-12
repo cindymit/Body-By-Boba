d1f404b9b2acdcf20f11c244b84805c212e001f4
Cindy Mit <cindymit@Cindys-MacBook-Air.local>
2021-05-12 09:56:30 -0400

    Added media query for phone and tablet

--- bobady/src/components/Nav.jsx
+++ bobady/src/components/Nav.jsx
@@ -6,11 +6,8 @@ function Nav() {
       <div className="img">
         <img src="https://ctl.s6img.com/society6/img/4DWapWcqIAdTgT5nAqRgNhxVrWo/w_700/prints/~artwork/s6-original-art-uploads/society6/uploads/misc/67eba195a53242caa0f3136212d98070/~~/cute-love-heart-bubble-tea-prints.jpg" alt="cute_boba" border="1px solid black" />
       </div>
-      <div className="app-title">
-        <h1>Body by Boba</h1>
-      </div>
       <div className="links">
-        <Link to="/" className="links">Homepage</Link>
+        <Link to="/home" className="links">Homepage</Link>
         <Link to="/filter" className="links">Filter</Link>
       </div>
     </nav>
