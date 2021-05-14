54ff53f07cfcb264e2b0b7a539553ad707b9ff51
Cindy Mit <cindymit@Cindys-MacBook-Air.local>
2021-05-14 09:35:03 -0400

    modified welcome jsx and css

--- bobady/src/components/Nav.jsx
+++ bobady/src/components/Nav.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <nav className="navbar">
-      <div className="img">
+      <div className="boba-logo">
         <img src="https://i.imgur.com/7sqOg1m.png" alt="boba_logo" />
       </div>
-      <div className="links">
+      <div className="link-container">
         <Link to="/home" className="links">Homepage</Link>
         <Link to="/filter" className="links">Filter</Link>
       </div>
--- bobady/src/components/Welcome.jsx
+++ bobady/src/components/Welcome.jsx
@@ -4,8 +4,8 @@ function Welcome() {
   return (
     <div className="welcome">
       <h1>Welcome to Body by Boba!</h1>
-      <Link to="/home"><img className="homepage-logo" src="https://i.imgur.com/jegll8w.png" alt="bobady_girl" /></Link>
-      <h1>Click on the logo to enter!</h1>
+      <Link to="/home"><img className="welcome-logo" src="https://i.imgur.com/jegll8w.png" alt="bobady_girl" /></Link>
+      <h2>Click on the logo to enter!</h2>
     </div>
   )
 }
